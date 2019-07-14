798494d98f3da6d34a5e664d47411a71c570bc42
ctq140109 <1401095999@qq.com>
2019-07-15 01:01:58 +0800

    城市索引选择

--- pages/area/area.js
+++ pages/area/area.js
@@ -50,25 +50,25 @@
 //   }
 // })
 const app = getApp();
+var wxaSortPicker = require('../../components/common/wxaSortPicker/wxaSortPicker.js');
 import {
   index_url
 } from '../../service/api.js';
 const http = app.globalData.http;
 Page({
   data: {
-    agency_list: [],
-    cities: []
+    agency_list: []
   },
-  onChange(event) {
-    console.log(event.detail, 'click right menu callback data')
-  },
-  onLoad() {
+  onLoad: function() {
     this.getCity();
   },
-  onReady() {
-
+  //选中返回值
+  wxaSortPickerItemTap: function(e) {
+    console.log(e.target.dataset.text);
+    console.log(e.target.dataset.value); //字符串数组无此字段
   },
   getCity() {
+    var that = this;
     http.request({
       url: index_url,
       method: 'POST',
@@ -77,33 +77,63 @@ Page({
       this.setData({
         agency_list: res.data.agency_list
       })
-      res.data.agency_list[0].pinyin = 'A';
-      res.data.agency_list[1].pinyin = 'P';
-      res.data.agency_list[2].pinyin = 'M';
-      res.data.agency_list[3].pinyin = 'Z';
-      this.filterCity(res.data.agency_list);
+      for (let i of res.data.agency_list){
+        i.value = i.id;
+      }
+      wxaSortPicker.init(res.data.agency_list, that);
     })
   },
-  filterCity(cities) {
-    let storeCity = new Array(26);
-    const words = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    words.forEach((item, index) => {
-      storeCity[index] = {
-        key: item,
-        list: []
-      }
-    })
-    cities.forEach((item) => {
-      let firstName = item.pinyin.substring(0, 1);
-      let index = words.indexOf(firstName);
-      storeCity[index].list.push({
-        name: item.name,
-        key: firstName
-      });
-    })
-    this.data.cities = storeCity;
-    this.setData({
-      cities: this.data.cities
-    })
-  }
-});
+})
+// Page({
+//   data: {
+//     agency_list: [],
+//     cities: []
+//   },
+//   onChange(event) {
+//     console.log(event.detail, 'click right menu callback data')
+//   },
+//   onLoad() {
+//     this.getCity();
+//   },
+//   onReady() {
+
+//   },
+//   getCity() {
+//     http.request({
+//       url: index_url,
+//       method: 'POST',
+//       data: {}
+//     }).then(res => {
+//       this.setData({
+//         agency_list: res.data.agency_list
+//       })
+//       res.data.agency_list[0].pinyin = 'A';
+//       res.data.agency_list[1].pinyin = 'P';
+//       res.data.agency_list[2].pinyin = 'M';
+//       res.data.agency_list[3].pinyin = 'Z';
+//       this.filterCity(res.data.agency_list);
+//     })
+//   },
+//   filterCity(cities) {
+//     let storeCity = new Array(26);
+//     const words = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//     words.forEach((item, index) => {
+//       storeCity[index] = {
+//         key: item,
+//         list: []
+//       }
+//     })
+//     cities.forEach((item) => {
+//       let firstName = item.pinyin.substring(0, 1);
+//       let index = words.indexOf(firstName);
+//       storeCity[index].list.push({
+//         name: item.name,
+//         key: firstName
+//       });
+//     })
+//     this.data.cities = storeCity;
+//     this.setData({
+//       cities: this.data.cities
+//     })
+//   }
+// });
